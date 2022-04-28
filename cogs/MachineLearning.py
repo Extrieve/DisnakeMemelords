0fbeb66d01c917ad0fc1186cbd870e1ee5b32d76
Nick <nicolas.cinera@gmail.com>
2022-04-28 00:11:56 -0400

    Continuing the Migration Process

---
+++ cogs/MachineLearning.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+from disnake.ext import commands
+import disnake
+import os, sys
+import requests
+
+class Machinelearning(commands.Cog):
+    """The description for Machinelearning goes here."""
+
+    cwd = os.getcwd()
+    sys.path.append(f'{cwd}..')
+    from config import google_key
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    def detect_labels_uri(self, uri):
+        """Detects labels in the file located in Google Cloud Storage or on the
+        Web."""
+        api_root = 'https://vision.googleapis.com/v1/images:annotate?key='
+
+        # Request parameters
+        params = {
+            'requests': [{
+                'image': {
+                    'source': {
+                        'imageUri': uri
+                    }
+                },
+                'features': [{
+                    'type': 'LABEL_DETECTION',
+                    'maxResults': 10
+                }]
+            }]
+        }
+
+        # make the request
+        r = requests.post(api_root + self.google_key, json=params)
+
+        # print the response
+        response = r.json()['responses'][0]['labelAnnotations']
+        output = []
+        for item in response:
+            output.append(f"{item['description']}: {round(item['score'], 2)}")
+
+        return output
+
+    
+    @commands.slash_command(name='label', description='Detect labels in an image.')
+    async def label(self, inter, image_url):
+        """Detects labels in the file located in Google Cloud Storage or on the
+        Web."""
+        labels = self.detect_labels_uri(image_url)
+        
+        # Embed the labels
+        embed = disnake.Embed(title='Labels', description='\n'.join(labels))
+        embed.set_image(url=image_url)
+        await inter.response.send_message(embed=embed)
+
+
+
+def setup(bot):
+    bot.add_cog(Machinelearning(bot))
--- cogs/Search.py
+++ cogs/Search.py
@@ -1,5 +1,4 @@
 from disnake.ext import commands
-from disnake.ui import Button
 import disnake
 import sys, os
 import requests
@@ -16,11 +15,13 @@ class Search(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     def getDefinition(self, word):
         response = requests.get(
             f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}')
         processedResponse = json.loads(response.text)
         return processedResponse[0]['meanings'][0]['definitions']
+
 
     @commands.slash_command(name='define', description='Get the definition of a word')
     async def define(self, inter, *, word):
@@ -69,8 +70,128 @@ class Search(commands.Cog):
             await msg.edit(embed=embed)
 
             await msg.remove_reaction(reaction.emoji, user)
-        
 
+
+    @commands.slash_command(name='image-search', description='Search for an image')
+    async def image_search(self, inter, query):
+
+        await inter.response.defer(with_message='Searching...')
+
+        params = {
+            'q': query,
+            'tbm': 'isch',
+            'ijn': '0',
+            'api_key': self.serp_key
+        }
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        length = len(results['images_results'])
+
+        if not results:
+            return await inter.followup.send(f'No results found for {search}.', ephimeral=True)
+    
+        # await ctx.send(results['images_results'][0]['original'])
+
+        # Display the first image but allow the user to traverse the rest
+        embed = disnake.Embed(title=f'{results["images_results"][0]["title"]}', url=results['images_results'][0]['original'], color=0x00ff00)
+        embed.set_image(url=results['images_results'][0]['original'])
+        await inter.followup.send(embed=embed)
+        msg = await inter.original_message()
+        # Allow the user to traverse the rest of the images
+        await msg.add_reaction('◀️')
+        await msg.add_reaction('▶️')
+
+        def check(reaction, user):
+            return user == inter.author and str(reaction.emoji) in ['◀️', '▶️']
+        
+        index = 0
+        while True:
+            try:
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
+            except asyncio.TimeoutError:
+                return await inter.followup.send('Request timed out.', ephimeral=True)
+            else:
+                if str(reaction.emoji) == '◀️':
+                    index -= 1
+                    if index < 0:
+                        index = length - 1
+                elif str(reaction.emoji) == '▶️':
+                    index += 1
+                    if index >= length:
+                        index = 0
+                embed = disnake.Embed(title=f'{results["images_results"][index]["title"]}', url=results['images_results'][index]['original'], color=0x00ff00)
+                embed.set_image(url=results['images_results'][index]['original'])
+                await msg.edit(embed=embed)
+                await msg.remove_reaction(reaction.emoji, user)
+
+    
+    @commands.slash_command(name='yugioh', description='Search for a card')
+    async def yugi(self, inter, search):
+        """
+        Search for a card on Yugioh Wiki.
+        """
+
+        url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+        params = {
+            'name': search
+        }
+        response = requests.get(url, params=params)
+
+        if response.status_code != 200:
+            return await inter.response.send_message('No results found.')
+
+        data = json.loads(response.text)
+        
+        images = []
+        for i in range(len(data['data'][0]['card_images'])):
+            images.append(data['data'][0]['card_images'][i]['image_url'])
+
+        embed = disnake.Embed(title=f'{data["data"][0]["name"]}', color=0x00ff00)
+        embed.set_image(url=images[0])
+        embed.add_field(name='Card Type', value=data['data'][0]['type'])
+        embed.add_field(name='Card Description', value=data['data'][0]['desc'])
+        embed.add_field(name='Card ATK', value=data['data'][0]['atk'])
+        embed.add_field(name='Card DEF', value=data['data'][0]['def'])
+        embed.add_field(name='Card Level', value=data['data'][0]['level'])
+        
+        await inter.response.send_message(embed=embed)
+        message = await inter.original_message()
+
+        # Add reactions to the message
+        await message.add_reaction('◀️')
+        await message.add_reaction('▶️')
+
+        # Wait for a reaction
+        def check(reaction, user):
+            return user == inter.author and str(reaction.emoji) in ['◀️', '▶️'] and reaction.message.id == message.id
+        
+        index = 0
+        while True:
+            try:
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
+            except asyncio.TimeoutError:
+                return await inter.followup.send('Timed out.')
+
+            if str(reaction.emoji) == '◀️':
+                index -= 1
+                if index < 0:
+                    index = len(images) - 1
+            elif str(reaction.emoji) == '▶️':
+                index += 1
+                if index >= len(images):
+                    index = 0
+
+            embed.clear_fields()
+            embed.set_image(url=images[index])
+            embed.add_field(name='Card Type', value=data['data'][0]['type'])
+            embed.add_field(name='Card Description', value=data['data'][0]['desc'])
+            embed.add_field(name='Card ATK', value=data['data'][0]['atk'])
+            embed.add_field(name='Card DEF', value=data['data'][0]['def'])
+            embed.add_field(name='Card Level', value=data['data'][0]['level'])
+            await message.edit(embed=embed)
+
+            await message.remove_reaction(reaction.emoji, user)
+        
 
 def setup(bot):
     bot.add_cog(Search(bot))
