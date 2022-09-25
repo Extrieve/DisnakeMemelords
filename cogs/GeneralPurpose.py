010a1be9325dd89f5da2b9cbf8a1c4f9a806d4e3
Nick <nicolas.cinera@gmail.com>
2022-09-25 14:05:41 -0400

    Video Compression for large Payloads
    - Exceeding 8mb without server boosts

--- cogs/GeneralPurpose.py
+++ cogs/GeneralPurpose.py
@@ -30,37 +30,89 @@ class GeneralPurpose(commands.Cog):
         # self.test1.start()
 
 
-    def compress_video(self, video_full_path, output_file_name, target_size):
-    # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+    def compress_video(self, video_full_path, size_upper_bound, two_pass=True, filename_suffix='cps_'):
+        """
+        Compress video file to max-supported size.
+        :param video_full_path: the video you want to compress.
+        :param size_upper_bound: Max video size in KB.
+        :param two_pass: Set to True to enable two-pass calculation.
+        :param filename_suffix: Add a suffix for new video.
+        :return: out_put_name or error
+        """
+        filename, extension = os.path.splitext(video_full_path)
+        print(filename)
+        extension = '.mp4'
+        output_file_name = filename + filename_suffix + extension
+
+        # Adjust them to meet your minimum requirements (in bps), or maybe this function will refuse your video!
+        total_bitrate_lower_bound = 11000
         min_audio_bitrate = 32000
         max_audio_bitrate = 256000
+        min_video_bitrate = 100000
 
-        probe = ffmpeg.probe(video_full_path)
-        # Video duration, in s.
-        duration = float(probe['format']['duration'])
-        # Audio bitrate, in bps.
-        audio_bitrate = float(next(
-            (s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
-        # Target total bitrate, in bps.
-        target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+        try:
+            # Bitrate reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+            probe = ffmpeg.probe(video_full_path)
+            # Video duration, in s.
+            duration = float(probe['format']['duration'])
+            # Audio bitrate, in bps.
+            audio_bitrate = float(next(
+                (s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+            # Target total bitrate, in bps.
+            target_total_bitrate = (
+                size_upper_bound * 1024 * 8) / (1.073741824 * duration)
+            if target_total_bitrate < total_bitrate_lower_bound:
+                print('Bitrate is extremely low! Stop compress!')
+                return False
 
-        # Target audio bitrate, in bps
-        if 10 * audio_bitrate > target_total_bitrate:
-            audio_bitrate = target_total_bitrate / 10
-            if audio_bitrate < min_audio_bitrate < target_total_bitrate:
-                audio_bitrate = min_audio_bitrate
-            elif audio_bitrate > max_audio_bitrate:
-                audio_bitrate = max_audio_bitrate
-        # Target video bitrate, in bps.
-        video_bitrate = target_total_bitrate - audio_bitrate
+            # Best min size, in kB.
+            best_min_size = (min_audio_bitrate + min_video_bitrate) * \
+                (1.073741824 * duration) / (8 * 1024)
+            if size_upper_bound < best_min_size:
+                print('Quality not good! Recommended minimum size:',
+                    '{:,}'.format(int(best_min_size)), 'KB.')
+                # return False
 
-        i = ffmpeg.input(video_full_path)
-        ffmpeg.output(i, os.devnull,
-                    **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
-                    ).overwrite_output().run()
-        ffmpeg.output(i, output_file_name,
-                    **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
-                    ).overwrite_output().run()
+            # Target audio bitrate, in bps.
+            audio_bitrate = audio_bitrate
+
+            # target audio bitrate, in bps
+            if 10 * audio_bitrate > target_total_bitrate:
+                audio_bitrate = target_total_bitrate / 10
+                if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+                    audio_bitrate = min_audio_bitrate
+                elif audio_bitrate > max_audio_bitrate:
+                    audio_bitrate = max_audio_bitrate
+
+            # Target video bitrate, in bps.
+            video_bitrate = target_total_bitrate - audio_bitrate
+            if video_bitrate < 1000:
+                print('Bitrate {} is extremely low! Stop compress.'.format(video_bitrate))
+                return False
+
+            i = ffmpeg.input(video_full_path)
+            if two_pass:
+                ffmpeg.output(i, os.devnull,
+                            **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                            ).overwrite_output().run()
+                ffmpeg.output(i, output_file_name,
+                            **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                            ).overwrite_output().run()
+            else:
+                ffmpeg.output(i, output_file_name,
+                            **{'c:v': 'libx264', 'b:v': video_bitrate, 'c:a': 'aac', 'b:a': audio_bitrate}
+                            ).overwrite_output().run()
+
+            if os.path.getsize(output_file_name) <= size_upper_bound * 1024:
+                return output_file_name
+            elif os.path.getsize(output_file_name) < os.path.getsize(video_full_path):  # Do it again
+                return compress_video(output_file_name, size_upper_bound)
+            else:
+                return False
+        except FileNotFoundError as e:
+            print('You do not have ffmpeg installed!', e)
+            print('You can install ffmpeg by reading https://github.com/kkroening/ffmpeg-python/issues/251')
+            return False
 
     @commands.slash_command(name='avatar', description='Get the avatar of a user.')
     async def avatar(self, inter, *, user: disnake.Member = None) -> None:
@@ -335,18 +387,16 @@ class GeneralPurpose(commands.Cog):
 
         stream.download(filename='youtube.mp4', output_path='db/')
 
-        try:
-            video = disnake.File('db/youtube.mp4', filename='youtube.mp4')
-            return await inter.followup.send(file=video, ephemeral=False)
+        # check if size is larger than 8mb
+        if os.path.getsize('db/youtube.mp4') > 8388608:
+            await inter.followup.send('Your video is too large to send normally, compressing your video...', ephemeral=True)
+            self.compress_video('db/youtube.mp4', 8 * 1000)
+            file = disnake.File('db/youtubecps_.mp4', filename='youtubecps_.mp4')
+            return await inter.followup.send(file=file, ephemeral=False)
 
-        except Exception as e:
-            
-            await inter.followup.send(f'Filesize is greater than 8mb converting, reducing the filesize\nStarting compression...', ephemeral=True)
+        file = disnake.File('db/youtube.mp4', filename='youtube.mp4')
 
-            self.compress_video('db/youtube.mp4', 'db/compressed_youtube1.mp4', 8)
-            video = disnake.File('db/compressed_youtube.mp4', filename='compressed_youtube.mp4')
-
-            return await inter.followup.send(file=video, ephemeral=False)
+        return await inter.followup.send(file=file, ephemeral=False)
 
 
     # @commands.slash_command(name='weather', description='Get the live weather of a city')
