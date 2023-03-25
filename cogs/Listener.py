f9001e8719d1eb2e170729c6677486b1c968bb0f
Nick <nicolas.cinera@gmail.com>
2023-03-25 09:30:05 -0400

    Trim YT Video when Embbeding

--- cogs/Listener.py
+++ cogs/Listener.py
@@ -7,7 +7,6 @@ class Listener(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        print('message received')
 
         if message.author == self.bot.user:
             return
