20ffe499637aa79fef54676dd1af6233d07b12aa
Nick <nicolas.cinera@gmail.com>
2023-11-23 19:08:10 -0500

    Only allow jueves from viernes

--- cogs/Listener.py
+++ cogs/Listener.py
@@ -12,7 +12,10 @@ class Listener(commands.Cog):
             return
         
         if 'jueves' in message.content.lower():
-            await message.reply('https://cdn.discordapp.com/attachments/862790378925850624/1170728291745927339/Hoy_no_es_jueves_mamaguevo_HOY_ES_VIERNES_-_HD.mp4?ex=655a18af&is=6547a3af&hm=7bd185a6ad76eb570e9fc38f6f051790b25d5dee72bb1eda07ca8cdaae4ff1b5&')
+            created_at = message.created_at
+
+            if created_at.weekday() == 4:
+                await message.reply('https://cdn.discordapp.com/attachments/862790378925850624/1170728291745927339/Hoy_no_es_jueves_mamaguevo_HOY_ES_VIERNES_-_HD.mp4?ex=655a18af&is=6547a3af&hm=7bd185a6ad76eb570e9fc38f6f051790b25d5dee72bb1eda07ca8cdaae4ff1b5&')
             return
         
         if 'twxtter.com' in message.content or 'fxtwitter.com' in message.content or 'vxtwitter.com' in message.content:
